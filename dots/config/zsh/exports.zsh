91cd26c4f51e0627500879a2b1f0ff5a228eed67
Enrique Gonzalez <enriikke@gmail.com>
2023-12-13 22:38:28 -0500

    export N_PREFIX and add bin dir to PATH

--- dots/config/zsh/exports.zsh
+++ dots/config/zsh/exports.zsh
@@ -14,3 +14,9 @@ export GREP_COLOR="3;33"
 export TYPEWRITTEN_COLOR_MAPPINGS="primary:#9580FF;secondary:#8AFF80;accent:#FFFF80;info_negative:#FF80BF;info_positive:#8AFF80;info_neutral_1:#FF9580;info_neutral_2:#FFFF80;info_special:#80FFEA"
 export FZF_DEFAULT_COMMAND="rg --files --hidden"
 export FZF_DEFAULT_OPTS="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
+# Node
+export N_PREFIX=$HOME/.n
+
+# Path
+export PATH=$PATH:$N_PREFIX/bin
