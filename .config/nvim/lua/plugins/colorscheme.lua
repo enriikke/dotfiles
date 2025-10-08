b5d15dbb34367bfb21a91af7e62e1543c37286c6
Enrique Gonzalez <enriikke@gmail.com>
2025-10-08 00:08:43 -0400

    Add Neovim configuration files and update setup script for LazyVim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      transparent_background = true,
+      auto_integrations = true,
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
+          end
+        end,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
