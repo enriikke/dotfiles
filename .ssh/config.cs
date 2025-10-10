cf4067670caec8a797d3ba99bac93445d1309c5a
Enrique Gonzalez <enriikke@gmail.com>
2025-10-10 09:22:58 -0400

    oops
    Signed-off-by: Enrique Gonzalez <enriikke@gmail.com>

--- .ssh/config.cs
+++
@@ -1,9 +0,0 @@
-Host cs.refactored-goldfish-4jpjrr67wq3qrvr.master
-	User vscode
-	ProxyCommand /opt/homebrew/bin/gh cs ssh -c refactored-goldfish-4jpjrr67wq3qrvr --stdio -- -i /Users/enriikke/.ssh/codespaces.auto
-	UserKnownHostsFile=/dev/null
-	StrictHostKeyChecking no
-	LogLevel quiet
-	ControlMaster auto
-	IdentityFile /Users/enriikke/.ssh/codespaces.auto
-
