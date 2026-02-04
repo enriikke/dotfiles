691008228eb4fe4ff9cc880946a41386924903ce
Enrique Gonzalez <enrique@hey.com>
2026-02-04 08:24:45 -0500

    fix(nvim): Simplify vim-tmux-navigator config
    Let the plugin set up its own keymaps instead of overriding them

--- home/.config/nvim/lua/plugins/tmux-navigator.lua
+++ home/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -2,19 +2,5 @@
 -- Uses the same C-h/j/k/l bindings in both
 return {
   "christoomey/vim-tmux-navigator",
-  lazy = false,
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-  },
-  keys = {
-    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
-  },
+  event = "VeryLazy",
 }
